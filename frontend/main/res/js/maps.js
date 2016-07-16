df08c3ecbd694a0c69447d7d5196b60cd1e24396
Johannes Teklote <johannes.teklote@web.de>
2016-07-16 22:34:01 +0200

    js code for map in its own file

--- frontend/main/res/js/maps.js
+++ frontend/main/res/js/maps.js
@@ -1,3 +1,43 @@
 /**
  * Created by Johannes Teklote on 16.07.2016.
  */
+var data = [
+    {"Titel": "Hallo", "Beschreibung": "blub", "x": "51.494229755747405", "y": "7.4204922026910936"},
+    {"Titel": "Hallo", "Beschreibung": "blub", "x": "51.474229755747405", "y": "7.4204922026910936"},
+    {"Titel": "Hallo", "Beschreibung": "blub", "x": "51.454229755747405", "y": "7.4204922026910936"},
+    {"Titel": "Hallo", "Beschreibung": "blub", "x": "51.434229755747405", "y": "7.4204922026910936"},
+    {"Titel": "Hallo", "Beschreibung": "blub", "x": "51.414229755747405", "y": "7.4204922026910936"}
+
+
+];
+
+var map;
+
+function initialize() {
+    var latlng = new google.maps.LatLng(51.494229755747405, 7.4204922026910936);
+    var mapOptions = {
+        zoom: 8,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+    for (var i = 0; i < data.length; i++) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data[i].x, data[i].y),
+            icon: '../res/img/marker_red.png'
+        });
+
+        marker.setMap(map);
+
+        google.maps.event.addListener(marker, 'click', function () {
+            /*
+             m.setIcon('../res/img/marker_red.png');
+             }*/
+            alert('set blau ' + i);
+            marker.setIcon('../res/img/marker_blue.png');
+        });
+    }
+}
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ frontend/main/res/js/sample.js
@@ -0,0 +1,18 @@
+  var sample_data = {
+    'location': {
+      'lat': "51.42108",
+      'lon': "7.57947",
+      'street': "Bürenbrucherweg 19",
+      'plz': "58239",
+      'city': 'Schwerte',
+      'country': 'DE',
+      'name': 'Yoga Vidya Zentrum'
+    },
+    'name': "Adam Bauer & The Love Keys",
+    'description': "Meditationsmusik",
+    'category': 'Konzerte',
+    'time_start': "2016-07-16T17:00:00+0200",
+    'time_end': "",
+    'photo': [],
+    'website': "http://www.coolibri.de/veranstaltungen/heute/adam-bauer-the-love-keys--/1669214.html"
+  }
